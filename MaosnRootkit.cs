43cf10638b6fc6b9778c1f8d69c2871af2ad23ca
Freemasonry <95870255+MasonGroup@users.noreply.github.com>
2024-08-20 11:55:15 +0300

    Create MaosnRootkit.cs

---
+++ MaosnRootkit.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Windows.Forms;
+
+class Program
+{
+    static void Main()
+    {
+        MessageBox.Show("Gangsta freemasonry made me do it", "SuperBooRat");
+        Environment.Exit(0);
+    }
+}
